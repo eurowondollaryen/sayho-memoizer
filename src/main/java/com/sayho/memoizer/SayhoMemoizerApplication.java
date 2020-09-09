1157eca992f2c01c0db766a78a0e1ee70885acad
Sayho <sehoakasayho@naver.com>
2020-09-09 23:59:32 +0900

    init

---
+++ src/main/java/com/sayho/memoizer/SayhoMemoizerApplication.java
@@ -0,0 +1,13 @@
+package com.sayho.memoizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SayhoMemoizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SayhoMemoizerApplication.class, args);
+	}
+
+}
