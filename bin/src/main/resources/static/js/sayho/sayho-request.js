8f1cc3a952750ac5dddff1bcb4c052c08220e07e
eurowondollaryen <sehoakasayho@naver.com>
2020-11-02 18:49:55 +0900

    login form init

---
+++ bin/src/main/resources/static/js/sayho/sayho-request.js
@@ -0,0 +1,131 @@
+/* GLOBAL VARS */
+var global_quiz_list = [];
+var global_question_list = [];
+
+//insert questions for a quiz
+const registQuestion = function(seq) {
+	var inpQuestion = document.getElementById("inpQuestion").value;
+	var inpAnswer = document.getElementById("inpAnswer").value;
+	if(inpQuestion === "") {
+		alert("질문을 입력해 주세요!");
+		return;
+	}
+	if(inpAnswer === "") {
+		alert("답변을 입력해 주세요!");
+		return;
+	}
+	
+	var requestData = {
+		"quizSeq" : seq,
+		"usrId" : "sehoakasayho",
+		"question" : inpQuestion,
+		"answer" : inpAnswer
+	};
+	
+	$.ajax({
+		type: "post",
+		url: "/quiz/registQuestion",
+		data: requestData,
+		success: function(result) {
+			console.log('insert question result : ' + result["data"]);
+			alert("질문 등록에 성공했습니다!");
+			//등록 후 refresh
+			requestQuizList(global_quiz_list);
+		},
+		error : function(xhr, textStatus, errorThrown) {
+			alert("failed to insert data...\n" + xhr.status + " " + xhr.statusText);
+		}
+	});
+}
+
+//insert quiz
+const registQuiz = function() {
+	var quizTitle = document.getElementById("inpTitle").value; 
+	if(quizTitle === "") {
+		alert("제목을 입력해 주세요!");
+	} else {
+		var requestData = {
+			"usrId" : "sehoakasayho",
+			"quizNm" : quizTitle
+		};
+		$.ajax({
+			type: "post",
+			url: "/quiz/regist",
+			data: requestData,
+			success: function(result) {
+				console.log('insert result : ' + result["data"]);
+				requestQuizList(global_quiz_list);
+			},
+			error : function(xhr, textStatus, errorThrown) {
+				alert("failed to insert data...\n" + xhr.status + " " + xhr.statusText);
+			}
+		});
+	}
+};
+
+//delete quiz
+const deleteQuiz = function(seq) {
+	if(confirm("정말 삭제하시겠어요?")) {
+		$.ajax({
+			type: "delete",
+			url: "/quiz/delete",
+			data: {
+				   "usrId" : "sehoakasayho",
+				   "seq" : seq
+				   },
+			success: function(result) {
+				console.log('delete success');
+				requestQuizList(global_quiz_list);
+			},
+			error : function(xhr, textStatus, errorThrown) {
+				alert("delete request failed...\n" + xhr.status + " " + xhr.statusText);
+			}
+		});
+	}
+};
+
+//select quiz list
+const requestQuizList = function(copy_arr) {
+	$.ajax({
+		type: "post",
+		url: "/quiz/list",
+		data: {"usrId" : "sehoakasayho"},
+		success: function(result) {
+			console.log('request success');
+			console.log(result);
+			for(var i = 0; i < result.data.length; ++i) {
+				copy_arr[i] = result.data[i];
+			}
+			printQuizList(result.data);
+		},
+		error : function(xhr, textStatus, errorThrown) {
+			alert("request failed...\n" + xhr.status + " " + xhr.statusText);
+		}
+	});
+};
+
+//select question list
+const requestQuestionList = function(quizSeq) {
+	$.ajax({
+		type: "post",
+		url: "/quiz/questionList",
+		data: {
+			   "usrId" : "sehoakasayho",
+			   "quizSeq" : quizSeq
+			   },
+		success: function(result) {
+			console.log('question list request success!');
+			console.log(result);
+			//function jsonArrayToTable is in index.html
+			document.querySelector("#questionListArea").innerHTML = jsonArrayToTable(result.data);
+			global_question_list = result.data;
+			showQuestionModal();
+			document.querySelector("#startQuizBtn").addEventListener("click", (e) => {
+				//TODO : 새 탭을 띄워서 퀴즈를 진행하는 방식으로. 새 탭에 global_question_list 데이터를 넘겨준다.
+			});
+		},
+		error : function(xhr, textStatus, errorThrown) {
+			alert("request failed...\n" + xhr.status + " " + xhr.statusText);
+		}
+	});
+};
--- src/main/java/com/sayho/memoizer/controller/MainController.java
+++ src/main/java/com/sayho/memoizer/controller/MainController.java
@@ -1,12 +1,17 @@
 package com.sayho.memoizer.controller;
 
+import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
+import java.net.URLEncoder;
+import java.net.UnknownHostException;
 import java.security.SecureRandom;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,34 +19,46 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
-	//FOR GENERATING RANDOM NUMBER
-	public String generateState() {
-		SecureRandom random = new SecureRandom();
-		return new BigInteger(130, random).toString();
-	}
+	private String CLIENT_ID = "a7c6RelyzCX6EJUpkB63"; //애플리케이션 클라이언트 아이디값";
+	private String CLI_SECRET = "EP5dXHm7rH"; //애플리케이션 클라이언트 시크릿값";
 	
+	/*
+	 * 메인화면(소개화면) - 여기서 로그인해서 /index로 이동할 수 있다. 
+	 */
 	@GetMapping("/")
-	public String index() {
+	public String mainPage() {
 		
 		return "/main.html";
 	}
 	
-	//CSRF 방지를 위한 토큰 생성 코드
-	@GetMapping("/user/login")
-	@ResponseBody
-	public String login(@RequestParam Map<String, Object> record, HttpServletRequest request) {
-		String state = generateState();
-		request.getSession().setAttribute("state", state);
-		return state;
-	}
-	
 	@GetMapping("/error")
-	public String error() {
+	public String errorPage() {
 		return "/error.html";
 	}
 	
-	@GetMapping("/test")
-	public String test() {
+	/*
+	 *  
+	 */
+	@GetMapping("/index")
+	public String indexPage() {
+		
 		return "/index.html";
+	}
+	
+	/*
+	 * 로그인 화면
+	 *  
+	 */
+	@GetMapping("/login")
+	public String loginPage(HttpSession session, Model model) throws UnsupportedEncodingException, UnknownHostException {
+		String redirectURI = URLEncoder.encode("http://localhost:8080/naver/callback1", "UTF-8");
+	    SecureRandom random = new SecureRandom();
+	    String state = new BigInteger(130, random).toString();
+	    String apiURL = "https://nid.naver.com/oauth2.0/authorize?response_type=code";
+	    apiURL += String.format("&client_id=%s&redirect_uri=%s&state=%s",
+	        CLIENT_ID, redirectURI, state);
+	    session.setAttribute("state", state);
+	    model.addAttribute("apiURL", apiURL);
+		return "/login.html";
 	}
 }
